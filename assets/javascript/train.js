3f0a0d36969f7a4624c03041834cf68f72479df2
Kim <monroek73@yahoo.com>
2016-11-08 17:37:50 -0800

    add

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -1,11 +1,4 @@
-
-
-//Initialize Firebase//
-
-
-<script src="https://www.gstatic.com/firebasejs/3.5.3/firebase.js"></script>
-<script>
-  // Initialize Firebase
+ // Initialize Firebase
   var config = {
     apiKey: "AIzaSyBb8qB4GMIc78BcdFrNGTHZRClIfiz_uKA",
     authDomain: "train-project-c4502.firebaseapp.com",
@@ -14,14 +7,12 @@
     messagingSenderId: "732543658967"
   };
   firebase.initializeApp(config);
-</script>
 
 //Create a variable to reference the database.
 
 var database = firebase.database();
 
 //Initial Values
-
 var trainName ="";
 var destination ="";
 var frequency ="";
@@ -29,7 +20,9 @@ var nextArrival="";
 var minutesAway="";
 
 //Capture clik button
-$("#addTrain").on("click", function(){
+$("#addTrain").on("click", function()
+{
+//Grabbed values from tex-boxes
 	trainName = $('#trainName').val().trim();
 	destination= $('#destination').val().trim();
 	frequency= $('#frequency').val().trim();
@@ -37,38 +30,64 @@ $("#addTrain").on("click", function(){
 	minutesAway = $('#minutesAway').val().trim();
 
 	//Code for setting values in the database
-
 	database.ref().set({
 		trainName: trainName,
 		destination: destination,
 		frequency: frequency,
 		nextArrival: nextArrival,
 		minutesAway: minutesAway,
-	})
-	// Don't refresh the page
+
+    //Code for setting the push
+   	database.ref().push({
+		trainName: trainName,
+		destination: destination,
+		frequency: frequency,
+		nextArrival: nextArrival,
+		minutesAway: minutesAway,
+       
+})
+   	// Clear all of the text-boxes
+   	$('#trainName').val("");
+   	$('#destination').val("");
+   	$('#frequency').val("");
+   	$('#nextArrival').val("");
+   	$('#minutesAway').val("");
+
+	// Refresh the page
 	return false;
 });
 
 //Firebase watcher + initial loader
 
-database.ref().on("value", function(snapshot)
+database.ref().on("child_added", function(childSnapshot)
+{
 	//console log
-	console.log(snapshot.val());
-	console.log(snapshot.value().trainName);
-	console.log(snapshot.value().destination);
-	console.log(snapshot.value().frequency);
-	console.log(snapshot.value().nextArrival);
-	console.log(snapshot.value().minutesAway);
+	console.log(childSnapshot.val());
+	console.log(childSnapshot.val().trainName);
+	console.log(childSnapshot.val().destination);
+	console.log(childSnapshot.val().frequency);
+	console.log(childSnapshot.val().nextArrival);
+	console.log(childSnapshot.val().minutesAway);
 
 //Change the HTML to reflect
-$("#trainName").html(snapshot.val().trainName);
-$("#destination").html(snapshot.val().destination);
-$("#frequency").html(snapshot.val().frequency);
-$("#nextArrival").html(snapshot.val().nextArrival);
-$("#minutesAway").html(snapshot.val().minutesAway);
+$("#trainName").html(childSnapshot.val().trainName);
+$("#destination").html(childSnapshot.val().destination);
+$("#frequency").html(childSnapshot.val().frequency);
+$("#nextArrival").html(childSnapshot.val().nextArrival);
+$("#minutesAway").html(childSnapshot.val().minutesAway);
 
 //Handle errors
-},(errorObject){
+},
+    function(errorObject){
 	console.log("Errors handled: " + errorObject.code)
-})
-}
+
+});
+
+dataRef.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function
+(
+ $("#trainName").html(snapshot.val().trainName);
+ $("#destination").html(snapshot.val().destination);
+ $("#frequency").html(snapshot.val().frequency);
+ $("#nextArrival").html(snapshot.val().nextArrival);
+ $("#minutesAway").html(snapshot.val().minutesAway);
+ )
